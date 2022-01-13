17f1d62432ff67bfc51f21c203d1d0326533fade
wylder.menezes <wylder.marcos@secult.ce.gov.br>
2022-01-12 23:52:33 -0300

    refactor: Projeto reestruturado, arquivos modificados.

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,7 @@
-import styles from "../styles/Home.module.css";
-import LoginPage from "./login";
-
 export default function Home() {
    return (
-      <div className={styles.container}>
-         <LoginPage />
+      <div>
+         asdas
       </div>
    );
 }
